7fdfc33c52f637dcc8c163664cbed7b768db904e
Matthew Weldon <matt@siro.ai>
2025-11-10 15:53:39 -0500

    Release v2.3.6 - Update SDK binary with latest improvements

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SiroSDK",
-            url: "https://github.com/Siro-ai/SiroSDK/releases/download/2.3.5/SiroSDK.xcframework.zip",
-            checksum: "ad21d6822f4786f20d109ffe930a4409c460b0f6dc83b684ebd34bb5c0437091"
+            url: "https://github.com/Siro-ai/SiroSDK/releases/download/2.3.6/SiroSDK.xcframework.zip",
+            checksum: "7167a089a1a9de342d49acc921c04db7ff123226b0e86c7aacb543c235a282fc"
         )
     ]
 ) 
